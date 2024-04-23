96418c7b5e2e9ce44f39dc2048ef0d39e8111d7a
Liam Brown <liamxxbrown@gmail.com>
2024-04-23 14:43:15 -0400

    Added the three required testing functions, all with the type and number of test cases also required.

--- tests/test_course_grader.py
+++ tests/test_course_grader.py
@@ -3,10 +3,31 @@ from course_grader import convert_to_letter_grade
 
 
 # TODO-1: Add test_exact_grade_boundaries() function
+def test_exact_boundaries():
+    assert convert_to_letter_grade(0) == "F"
+    assert convert_to_letter_grade(59) == "F"
+    assert convert_to_letter_grade(60) == "D"
+    assert convert_to_letter_grade(70) == "C"
+    assert convert_to_letter_grade(79) == "C"
+    assert convert_to_letter_grade(80) == "B"
+    assert convert_to_letter_grade(89) == "B"
+    assert convert_to_letter_grade(90) == "A"
+    assert convert_to_letter_grade(100) == "A"
 
 
 # TODO-2: Add test_invalid_numerical_score() function
+def test_invalid_numerical_score():
+    with pytest.raises(ValueError):
+        convert_to_letter_grade(-1)
+    with pytest.raises(ValueError):
+        convert_to_letter_grade(101)
 
 
 # TODO-3: Add test_non_numeric_input() function
-
+def test_non_numeric_input():
+    with pytest.raises(TypeError):
+        convert_to_letter_grade("A")
+    with pytest.raises(TypeError):
+            convert_to_letter_grade([0, 59, 90])
+    with pytest.raises(TypeError):
+            convert_to_letter_grade(None)
